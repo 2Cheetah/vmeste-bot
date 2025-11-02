98be649ed9c6ffb8eb40e3f16615cb6cd90c2c99
Denis Safiullin <fckninja@gmail.com>
2025-11-02 20:52:34 +0300

    Update .gitignore to include .env files; refactor Makefile and index.ts for clarity and structure

--- Makefile
+++ Makefile
@@ -1,4 +1,11 @@
-.PHONY: serve-webhook
+.PHONY: start-supabase serve-webhook test-webhook
+.DEFAULT_GOAL=serve-webhook
 
-serve-webhook:
-	supabase functions serve webhook --no-verify-jwt
+start-supabase:
+	supabase start
+
+serve-webhook: start-supabase
+	supabase functions serve webhook --no-verify-jwt --env-file=supabase/functions/.env
+
+test-webhook:
+	deno test --allow-all --env-file=./supabase/functions/tests/.env.local ./supabase/functions/tests/webhook-test.ts
--- supabase/functions/webhook/index.ts
+++ supabase/functions/webhook/index.ts
@@ -1,12 +1,7 @@
-// Follow this setup guide to integrate the Deno language server with your editor:
-// https://deno.land/manual/getting_started/setup_your_environment
-// This enables autocomplete, go to definition, etc.
-
-// Setup type definitions for built-in Supabase Runtime APIs
 import "edge-runtime";
 import { Bot, webhookCallback } from "grammy";
 
-const bot = new Bot(Deno.env.get("TELEGRAM_BOT_TOKEN") || "");
+const bot = new Bot(Deno.env.get("TELEGRAM_BOT_TOKEN")!);
 
 bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
 
@@ -18,7 +13,9 @@ Deno.serve(async (req: Request) => {
   try {
     const url = new URL(req.url);
     if (url.searchParams.get("secret") !== Deno.env.get("FUNCTION_SECRET")) {
-      return new Response("not allowed", { status: 405 });
+      return new Response("not allowed", {
+        status: 405,
+      });
     }
 
     return await handleUpdate(req);
@@ -29,15 +26,3 @@ Deno.serve(async (req: Request) => {
     });
   }
 });
-
-/* To invoke locally:
-
-  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
-  2. Make an HTTP request:
-
-  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/webhook' \
-    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
-    --header 'Content-Type: application/json' \
-    --data '{"name":"Functions"}'
-
-*/
