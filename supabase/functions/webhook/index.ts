7b58f97800a45482213af51b294d53e3081bcfc0
Denis Safiullin <fckninja@gmail.com>
2025-11-09 20:41:55 +0800

    add db types, enhance buying handler

--- supabase/functions/webhook/index.ts
+++ supabase/functions/webhook/index.ts
@@ -1,6 +1,8 @@
 import "edge-runtime";
 import { createClient } from "supabase";
 import { Bot, Context, webhookCallback } from "grammy";
+import { User } from "grammy_types";
+import { Database } from "../../database.types.ts";
 
 const SUPABASE_URL = Deno.env.get("SUPABASE_URL");
 const SUPABASE_SERVICE_ROLE_KEY = Deno.env.get("SUPABASE_SERVICE_ROLE_KEY");
@@ -9,7 +11,7 @@ if (!SUPABASE_URL || !SUPABASE_SERVICE_ROLE_KEY) {
   throw new Error("Missing mandatory envs");
 }
 
-const supabase = createClient(
+const supabase = createClient<Database>(
   SUPABASE_URL!,
   SUPABASE_SERVICE_ROLE_KEY!,
 );
@@ -36,22 +38,23 @@ bot.command(
 bot.command("buy", buyHandler);
 
 async function buyHandler(ctx: Context) {
-  // Check for existing season tickets
-  const userId = ctx.message?.from.id;
   try {
-    const { data, error } = await supabase
-      .from("users")
-      .select("*")
-      .eq("id", userId);
+    const u: User = ctx.message?.from!;
 
-    if (error) {
-      throw error;
+    if (!await isUserExist(u.id)) {
+      ctx.reply("User not found in DB. Adding...");
+      await registerUser(u);
     }
 
-    if (!data) {
-      // Register the user
-      ctx.reply("UserId not found in DB");
+    const t = await getSeasonTicket(u);
+    if (t) {
+      const lessonsLeft = t.lessons_total - t.lessons_used;
+      return ctx.reply(
+        `You still have a valid season ticket with ${lessonsLeft} lessons left.`,
+      );
     }
+
+    await buyTicket(u);
   } catch (err) {
     console.error(err);
     ctx.reply(
@@ -61,6 +64,65 @@ async function buyHandler(ctx: Context) {
 
   // Create a record in DB
   return await ctx.reply("You successfully bought a season ticket!");
+}
+
+async function isUserExist(userId: number) {
+  const { data, error } = await supabase
+    .from("users")
+    .select("*")
+    .eq("id", userId);
+
+  if (error) {
+    throw error;
+  }
+
+  if (!data[0]) {
+    return false;
+  }
+
+  return true;
+}
+
+async function registerUser(u: User) {
+  const { error } = await supabase
+    .from("users")
+    .insert({
+      id: u.id,
+      username: u.username || "",
+    });
+
+  if (error) {
+    console.error("couldn't insert the user to the DB");
+    throw error;
+  }
+}
+
+async function getSeasonTicket(u: User) {
+  const { data, error } = await supabase
+    .from("tickets")
+    .select("*")
+    .eq("userId", u.id);
+
+  if (error) {
+    throw error;
+  }
+
+  if (data[0]) {
+    return data[0];
+  }
+}
+
+async function buyTicket(u: User) {
+  const { error } = await supabase
+    .from("tickets")
+    .insert({
+      user_id: u.id,
+      lessons_total: 4,
+    });
+
+  if (error) {
+    throw error;
+  }
 }
 
 const handleUpdate = webhookCallback(bot, "std/http");
